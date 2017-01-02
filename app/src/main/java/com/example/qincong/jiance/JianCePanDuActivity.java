74a0ffd6bc3f02c148913cb74765d2f21566cad5
秦聪 <helloqincong@gmail.com>
2017-01-02 22:21:12 +0800

    图像对比准确度不足

--- app/src/main/java/com/example/qincong/jiance/JianCePanDuActivity.java
+++ app/src/main/java/com/example/qincong/jiance/JianCePanDuActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -42,6 +45,7 @@ public class JianCePanDuActivity extends AppCompatActivity {
     public static final int MESSAGE_DEVICE_NAME = 4;
     public static final int MESSAGE_TOAST = 5;
     Gson gson;
+    public static Camera camera = Camera.open();
     private List<Info> infoList = null;
 
     @Override
@@ -50,7 +54,7 @@ public class JianCePanDuActivity extends AppCompatActivity {
         setContentView(R.layout.jiancepandu);
         gson = new Gson();
         FileInputStream inputStream = null;
-        infoList=new ArrayList<Info>();
+        infoList = new ArrayList<Info>();
         try {
             inputStream = getApplicationContext().openFileInput("json.txt");
             InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "utf-8");
@@ -59,8 +63,10 @@ public class JianCePanDuActivity extends AppCompatActivity {
             if (json != null) {
                 infoList = gson.fromJson(json, new TypeToken<List<Info>>() {
                 }.getType());
-            }
-            else {
+                if(infoList==null){
+                    infoList=new ArrayList<Info>();
+                }
+            } else {
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -80,33 +86,20 @@ public class JianCePanDuActivity extends AppCompatActivity {
                                 break;
                             case BlueToothService.SUCCESS_CONNECT:
                                 Toast.makeText(
-                                        JianCePanDuActivity.this, "a", Toast.LENGTH_SHORT).show();
-//                                vg.getChildAt(0).setVisibility(View.GONE);
-//                                vg.getChildAt(1).setVisibility(View.GONE);
-//                                vg.getChildAt(2).setVisibility(View.VISIBLE);
-//                                vg.getChildAt(2).setFocusable(true);
-//                                vg.getChildAt(2).setFocusableInTouchMode(true);
+                                        JianCePanDuActivity.this, "连接成功", Toast.LENGTH_SHORT).show();
 
                                 break;
                             case BlueToothService.FAILED_CONNECT:
                                 Toast.makeText(
                                         JianCePanDuActivity.this,
-                                        "str_faileconnect", Toast.LENGTH_SHORT)
+                                        "连接失败", Toast.LENGTH_SHORT)
                                         .show();
-//                                vg.getChildAt(0).setVisibility(View.VISIBLE);
-//                                vg.getChildAt(1).setVisibility(View.VISIBLE);
-//                                vg.getChildAt(2).setVisibility(View.GONE);
-//                                vg.getChildAt(2).setFocusable(false);
                                 break;
                             case BlueToothService.LOSE_CONNECT:
                                 switch (msg.arg2) {
                                     case -1:
                                         Toast.makeText(
                                                 JianCePanDuActivity.this, "str_lose", Toast.LENGTH_SHORT).show();
-//                                        vg.getChildAt(0).setVisibility(View.VISIBLE);
-//                                        vg.getChildAt(1).setVisibility(View.VISIBLE);
-//                                        vg.getChildAt(2).setVisibility(View.GONE);
-//                                        vg.getChildAt(2).setFocusable(false);
                                         break;
                                     case 0:
                                         break;
@@ -123,91 +116,194 @@ public class JianCePanDuActivity extends AppCompatActivity {
                 }
             }
         });
-        mBTService.ConnectToDevice("00:0D:18:00:1A:53");
-
+        if (!mBTService.IsOpen()) {// 判断蓝牙是否打开
+            mBTService.OpenDevice();
+        }
+        if (mBTService.getState() != mBTService.STATE_CONNECTED) {
+            mBTService.ConnectToDevice("00:0D:18:00:1A:53");
+            Toast.makeText(JianCePanDuActivity.this, "连接中", Toast.LENGTH_SHORT).show();
+        }
         ((Button) findViewById(R.id.kaishijiance)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent, 1);
+                Camera.Parameters parameters = camera.getParameters();
+                parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+                parameters.setExposureCompensation(3);
+                parameters.set("iso","800");
+                camera.setParameters(parameters);
+                camera.startPreview();
+                camera.takePicture(null, null, new TakePictureCallback());
+            }
+        });
+        ((Button) findViewById(R.id.jiaozhunyangxing)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Camera.Parameters parameters = camera.getParameters();
+                parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+                parameters.setExposureCompensation(3);
+                parameters.set("iso","800");
+                camera.setParameters(parameters);
+                camera.startPreview();
+                camera.takePicture(null, null, new TakePictureCallbackYang());
+            }
+        });
+        ((Button) findViewById(R.id.jiaozhunyinxing)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Camera.Parameters parameters = camera.getParameters();
+                parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+                parameters.setExposureCompensation(3);
+                parameters.set("iso","800");
+                camera.setParameters(parameters);
+                camera.startPreview();
+                camera.takePicture(null, null, new TakePictureCallbackYin());
             }
         });
         ((Button) findViewById(R.id.print)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (mBTService.getState() != mBTService.STATE_CONNECTED) {
+                    mBTService.DisConnected();
+                    mBTService.ConnectToDevice("00:0D:18:00:1A:53");
                     Toast.makeText(
                             JianCePanDuActivity.this, "未连接", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
-                mBTService.PrintCharacters("公司名称：XXXX\r\n及访问呢" + "\r\n");
-            }
-        });
+    mBTService.PrintCharacters("公司名称：XXXX\r\n及访问呢" + "\r\n");
+}
+});
+        }
+
+    private final class TakePictureCallback implements Camera.PictureCallback {
+        public void onPictureTaken(byte[] data, Camera camera) {
+            Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+            bitmap = Bitmap.createBitmap(bitmap, 200, 570, 300, 100);
+            Matrix matrix=new Matrix();
+            matrix.postRotate(90);
+            bitmap=Bitmap.createBitmap(bitmap,0,0,bitmap.getWidth(),bitmap.getHeight(),matrix,true);
+            doBitmap(bitmap);
+        }
+    }
+    private final class TakePictureCallbackYang implements Camera.PictureCallback {
+        public void onPictureTaken(byte[] data, Camera camera) {
+            Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+            bitmap = Bitmap.createBitmap(bitmap, 200, 570, 300, 100);
+            Matrix matrix=new Matrix();
+            matrix.postRotate(90);
+            bitmap=Bitmap.createBitmap(bitmap,0,0,bitmap.getWidth(),bitmap.getHeight(),matrix,true);
+            doBitmapYang(bitmap);
+            ((ImageView) findViewById(R.id.imageView)).setImageBitmap(bitmap);
+        }
+    }
+    private final class TakePictureCallbackYin implements Camera.PictureCallback {
+        public void onPictureTaken(byte[] data, Camera camera) {
+            Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+            bitmap = Bitmap.createBitmap(bitmap, 200, 570, 300, 100);
+            Matrix matrix=new Matrix();
+            matrix.postRotate(90);
+            bitmap=Bitmap.createBitmap(bitmap,0,0,bitmap.getWidth(),bitmap.getHeight(),matrix,true);
+            doBitmapYin(bitmap);
+            ((ImageView) findViewById(R.id.imageView)).setImageBitmap(bitmap);
+        }
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        // TODO Auto-generated method stub
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == Activity.RESULT_OK) {
-            Bundle bundle = data.getExtras();
-            Bitmap bitmap = (Bitmap) bundle.get("data");// 获取相机返回的数据，并转换为Bitmap图片格式
-            FileOutputStream fileOutputStream = null;
-            String name = null;
-            try {
-                name = new DateFormat().format("yyyyMMdd_hhmmss", Calendar.getInstance(Locale.CHINA)) + ".jpg";
-                fileOutputStream = JianCePanDuActivity.this.openFileOutput(name, MODE_PRIVATE);
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream);
-                fileOutputStream.close();
-
-                File file = new File(getApplicationContext().getFilesDir().getPath() + "/img.txt");
-                if (!file.exists()) {
-                    file.createNewFile();
-                }
-                FileOutputStream file_img = getApplicationContext().openFileOutput("img.txt", MODE_APPEND);
-                file_img.write((name + "\n").getBytes("utf-8"));
-                file_img.close();
-            } catch (Exception myexception) {
-                myexception.printStackTrace();
-            }
-            ((ImageView) findViewById(R.id.imageView)).setImageBitmap(bitmap);// 将图片显示在ImageView里
-            Info info = new Info();
-            try {
-                FileInputStream inputStream = getApplicationContext().openFileInput("yangbenxinxi.txt");
-                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "utf-8");
-                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-                String str;
-                info.img = name;
-                info.result = "yang";
-                info.chouyangdanwei = bufferedReader.readLine();
-                info.yangpinmingcheng = bufferedReader.readLine();
-                info.jiancexiangmu = bufferedReader.readLine();
-                info.jiancebeizhu = bufferedReader.readLine();
-                info.CTzhishezhi = bufferedReader.readLine();
-                info.jianceleixing = bufferedReader.readLine();
-                info.tijiaoleixing = bufferedReader.readLine();
-                inputStream = getApplicationContext().openFileInput("usr_info.txt");
-                inputStreamReader = new InputStreamReader(inputStream, "utf-8");
-                bufferedReader = new BufferedReader(inputStreamReader);
-                info.yonghumingcheng = bufferedReader.readLine();
-                info.yonghuxingbie = bufferedReader.readLine();
-                info.youxiangdizhi = bufferedReader.readLine();
-                info.shouji = bufferedReader.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            infoList.add(info);
-            String json = gson.toJson(infoList);
-            FileOutputStream file_img = null;
-            try {
-                file_img = getApplicationContext().openFileOutput("json.txt", MODE_PRIVATE);
-                file_img.write(json.getBytes("utf-8"));
-                file_img.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+    public void onStop() {
+        super.onStop();
+        if (mBTService.getState() == mBTService.STATE_CONNECTED) {
+            mBTService.DisConnected();
+            Toast.makeText(JianCePanDuActivity.this, "断开连接", Toast.LENGTH_SHORT).show();
         }
     }
+    protected void doBitmapYang(Bitmap bitmap) {
+        FileOutputStream fileOutputStream = null;
+        String name = null;
+        try {
+            name = "bz_yang.jpg";
+            fileOutputStream = JianCePanDuActivity.this.openFileOutput(name, MODE_PRIVATE);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream);
+            fileOutputStream.close();
+        }catch (Exception myexception) {
+            myexception.printStackTrace();
+        }
+    }
+    protected void doBitmapYin(Bitmap bitmap) {
+        FileOutputStream fileOutputStream = null;
+        String name = null;
+        try {
+            name = "bz_yin.jpg";
+            fileOutputStream = JianCePanDuActivity.this.openFileOutput(name, MODE_PRIVATE);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream);
+            fileOutputStream.close();
+        }catch (Exception myexception) {
+            myexception.printStackTrace();
+        }
+    }
+
+    protected void doBitmap(Bitmap bitmap) {
+        FileOutputStream fileOutputStream = null;
+        String name = null;
+        try {
+            name = new DateFormat().format("yyyyMMdd_hhmmss", Calendar.getInstance(Locale.CHINA)) + ".jpg";
+            fileOutputStream = JianCePanDuActivity.this.openFileOutput(name, MODE_PRIVATE);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream);
+            fileOutputStream.close();
+
+            File file = new File(getApplicationContext().getFilesDir().getPath() + "/img.txt");
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            FileOutputStream file_img = getApplicationContext().openFileOutput("img.txt", MODE_APPEND);
+            file_img.write((name + "\n").getBytes("utf-8"));
+            file_img.close();
+        } catch (Exception myexception) {
+            myexception.printStackTrace();
+        }
+        ((ImageView) findViewById(R.id.imageView)).setImageBitmap(bitmap);// 将图片显示在ImageView里
+        Info info = new Info();
+        try {
+            FileInputStream inputStream = getApplicationContext().openFileInput("yangbenxinxi.txt");
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "utf-8");
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String str;
+            info.img = name;
+            Double yang = BitmapCompare.similarity(getFilesDir() + "/bz_yang.jpg", getFilesDir() + "/" + name);
+            Double yin = BitmapCompare.similarity(getFilesDir() + "/bz_yin.jpg", getFilesDir() + "/" + name);
+            String line = yang.toString();
+            if (yang > yin) {
+                info.result = "yang";
+            } else {
+                info.result = "yin";
+            }
+            Toast.makeText(getApplicationContext(), info.result+yang+"  "+yin, Toast.LENGTH_SHORT).show();
+            info.chouyangdanwei = bufferedReader.readLine();
+            info.yangpinmingcheng = bufferedReader.readLine();
+            info.jiancexiangmu = bufferedReader.readLine();
+            info.jiancebeizhu = bufferedReader.readLine();
+            info.CTzhishezhi = bufferedReader.readLine();
+            info.jianceleixing = bufferedReader.readLine();
+            info.tijiaoleixing = bufferedReader.readLine();
+            inputStream = getApplicationContext().openFileInput("usr_info.txt");
+            inputStreamReader = new InputStreamReader(inputStream, "utf-8");
+            bufferedReader = new BufferedReader(inputStreamReader);
+            info.yonghumingcheng = bufferedReader.readLine();
+            info.yonghuxingbie = bufferedReader.readLine();
+            info.youxiangdizhi = bufferedReader.readLine();
+            info.shouji = bufferedReader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        infoList.add(info);
+        String json = gson.toJson(infoList);
+        FileOutputStream file_img = null;
+        try {
+            file_img = getApplicationContext().openFileOutput("json.txt", MODE_PRIVATE);
+            file_img.write(json.getBytes("utf-8"));
+            file_img.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
